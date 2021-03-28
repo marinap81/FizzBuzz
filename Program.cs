c44cf85224b1f815cf92e5ec08de9b3c0a7c011a
Marina Pajvancic <103340660@student.swin.edu.au>
2021-03-28 18:33:20 +1100

    completed assessment

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i = i + 1)
+            {
+               int mod3 = i % 3; // same as i divided by 3 but give me just the numbers after the decimal, % means modulus
+               int mod5 = i % 5;
+
+               if ( mod3 == 0 && mod5 == 0 )
+               {
+                  Console.WriteLine("FizzBuzz");
+               }
+               else if ( mod3 == 0 )  // When there ir no remainder (ie. 9/3 = 3.0 ) there is a zero after the decimal, so print Fizz. 
+               {
+                  Console.WriteLine("Fizz");    
+               }
+               else if ( mod5 == 0 )
+               {
+                  Console.WriteLine("Buzz");
+               }
+               else
+               {
+                  Console.WriteLine(i);
+               }
+            }
+        }
+    }
+}
